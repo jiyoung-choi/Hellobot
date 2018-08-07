dda4ff88ae077096f64e4e326618fce1f0b619bd
최 지영 <jiyoung031@gmail.com>
2018-08-07 16:08:44 +0900

    slack bot

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-print('Hello World!')
-print("웅앵웅?")
-print("초키포키")
-print('웅앵웅')
+from rtmbot import RtmBot
+from secret import SLACK_TOKEN
+from rtmbot.core import Plugin
+
+class HelloPlugin(Plugin):
+    def process_message(self, data):
+        if "?" in data["text"]:
+            self.outputs.append([data["channel"], "Hello, World!"])
+
+config = {
+    "SLACK_TOKEN": secret.SLACK_TOKEN,
+    "ACTIVE_PLUGINS": ["main.HelloPlugin"]
+}
+
+bot = RtmBot(config)
+bot.start()
